bce3a5e180795e0c377a56b6e38acf7eba2ecbb0
Maksims Saldajevs <maksimsf.saldajevs@gmail.com>
2022-10-08 18:34:18 +0100

    first commit

---
+++ Q24A.java
@@ -0,0 +1,15 @@
+//Answer to exercise 24 A
+public class Q24A {
+    
+    public static String gauss(int n){
+        int total = 1; 
+        
+        for(int i = 2; i<=n;i++){
+            total+=i;
+        }
+        
+        return "Total: " + total;
+    
+    }
+    
+}
